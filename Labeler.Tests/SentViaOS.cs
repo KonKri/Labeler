fe795338e7591604946ee6c52d1d1b474dbcfcb1
Konstantinos Kritikakis <kkritikakis@people-techsolutions.com>
2021-01-17 22:55:45 +0200

    added some unit tests

---
+++ Labeler.Tests/SentViaOS.cs
@@ -0,0 +1,22 @@
+﻿using Labeler.Core.Attributes;
+
+namespace Labeler.Tests
+{
+    enum SentViaOS
+    {
+        [Label("Sent via iOS 12")]
+        [Label("Sent via iPhone 13")]
+        [Label("Sent via iPhone 14")]
+        SentViaIOS,
+
+        [Label("Sent via Android 9")]
+        [Label("Sent via Android 10")]
+        [Label("Sent via Android 11")]
+        SentViaAndroid,
+
+        [Label("Send via Windows")]
+        SentViaWindows,
+
+        SentViaUbuntu
+    }
+}
---
+++ Labeler.Tests/TestExamples.cs
@@ -0,0 +1,46 @@
+using Labeler.Core.Extentions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Labeler.Tests
+{
+    [TestClass]
+    public class TestExamples
+    {
+        private string[] SentViaOsLabels;
+
+        public TestExamples()
+        {
+            SentViaOsLabels = new string[] { "Sent via iOS 12", "Sent via iPhone 13", "Sent via iPhone 14" };
+        }
+
+        [TestMethod]
+        public void TryGetMultipleLabels_MultipleLabelsExist_ReturnLabelsAsEnumerable()
+        {
+            var labels = SentViaOS.SentViaIOS.GetLabels();
+
+            Assert.IsTrue(labels.Intersect(SentViaOsLabels).Count() == 3);
+        }
+
+        [TestMethod]
+        public void TryGetLabel_MultipleLabelsExist_ReturnFirstLabel()
+        {
+            Assert.AreEqual(SentViaOS.SentViaAndroid.GetLabel(), "Sent via Android 9");
+        }
+
+        [TestMethod]
+        public void TryGetLabel_LabelNotExists_ReturnsFieldAsStr()
+        {
+            Assert.AreEqual(SentViaOS.SentViaUbuntu.GetLabel(), "SentViaUbuntu");
+        }
+
+        [TestMethod]
+        public void TryGetMultipleLabels_LabelsNotExist_ReturnsFieldAsStr()
+        {
+            var labels = SentViaOS.SentViaUbuntu.GetLabels();
+            
+            Assert.IsTrue(labels.Count() == 1);
+            Assert.AreEqual(labels.First(), "SentViaUbuntu");
+        }
+    }
+}
