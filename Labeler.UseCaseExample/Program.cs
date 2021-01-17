289fdd1da40552f2f7c2f367009736204d3d8ba1
Konstantinos Kritikakis <kkritikakis@people-techsolutions.com>
2021-01-17 22:24:07 +0200

    add examples.

---
+++ Labeler.UseCaseExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using Labeler.Core.Extentions;
+using System;
+using System.Linq;
+
+namespace Labeler.UseCaseExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // defining our steak's levels.
+            var mySteakDonenessLevel = SteakDoneness.MediumRare;
+            var dimitrisSteakDonenessLevel = SteakDoneness.Medium;
+            var mariaSteakDonenessLevel = SteakDoneness.WellDone;
+
+            // displaying a story about the steaks we had.
+            Console.WriteLine($"Yesterday, I ordered a {mySteakDonenessLevel.GetLabel().ToLower()} steak, " + // upon get label as string you can act on it as you wish.
+                              $"while Dimitris had a {dimitrisSteakDonenessLevel.GetLabel().ToLower()} steak, " + // there is no label for Medium. Thus "Medium" will be returned.
+                              $"and Maria had a {mariaSteakDonenessLevel.GetLabels().Last().ToLower()} one."); // there are multiple labels for WellDone. You can select which one you want to use.
+        }
+    }
+}
---
+++ Labeler.UseCaseExample/SteakDoneness.cs
@@ -0,0 +1,25 @@
+﻿using Labeler.Core.Attributes;
+
+namespace Labeler.UseCaseExample
+{
+    /// <summary>
+    /// This an example of a steak doneness levels.
+    /// Notice we are using the label attribute wherever it's nessesary.
+    /// </summary>
+    enum SteakDoneness
+    {
+        Rare,
+        
+        [Label("Medium Rare")]
+        MediumRare,
+        
+        Medium,
+        
+        [Label("Medium Rare")]
+        MediumWell,
+
+        [Label("Well Done")]
+        [Label("Well")] // "well done" level can be found just as "well" as well...
+        WellDone,
+    }
+}
