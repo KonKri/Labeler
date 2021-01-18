76f5fd3a12966301c3ae42cbfb7f0cfb480dc8e2
Dim Grev <dim.grev@outlook.com>
2021-01-18 14:29:55 +0200

    Adding returned msg in TestExamples

--- Labeler.Tests/TestExamples.cs
+++ Labeler.Tests/TestExamples.cs
@@ -1,6 +1,7 @@
 using Labeler.Core.Extentions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
+using System.Reflection;
 
 namespace Labeler.Tests
 {
@@ -11,36 +12,35 @@ namespace Labeler.Tests
 
         public TestExamples()
         {
-            SentViaOsLabels = new string[] { "Sent via iOS 12", "Sent via iPhone 13", "Sent via iPhone 14" };
+            SentViaOsLabels = new string[] { "Sent via IPhone 12", "Sent via iOS 13", "Sent via iOS 14" };
         }
 
         [TestMethod]
         public void TryGetMultipleLabels_MultipleLabelsExist_ReturnLabelsAsEnumerable()
         {
             var labels = SentViaOS.SentViaIOS.GetLabels();
-
-            Assert.IsTrue(labels.Intersect(SentViaOsLabels).Count() == 3);
+            Assert.IsTrue(labels.Intersect(SentViaOsLabels).Count() == 3, $"TestMethod: {MethodBase.GetCurrentMethod().Name} Failed.");
         }
 
         [TestMethod]
         public void TryGetLabel_MultipleLabelsExist_ReturnFirstLabel()
         {
-            Assert.AreEqual(SentViaOS.SentViaAndroid.GetLabel(), "Sent via Android 9");
+            Assert.AreEqual(SentViaOS.SentViaAndroid.GetLabel(), "Sent via Android 9", $"TestMethod: {MethodBase.GetCurrentMethod().Name} Failed.");
         }
 
         [TestMethod]
         public void TryGetLabel_LabelNotExists_ReturnsFieldAsStr()
         {
-            Assert.AreEqual(SentViaOS.SentViaUbuntu.GetLabel(), "SentViaUbuntu");
+            Assert.AreEqual(SentViaOS.SentViaUbuntu.GetLabel(), "SentViaUbuntu", $"TestMethod: {MethodBase.GetCurrentMethod().Name} Failed.");
         }
 
         [TestMethod]
         public void TryGetMultipleLabels_LabelsNotExist_ReturnsFieldAsStr()
         {
             var labels = SentViaOS.SentViaUbuntu.GetLabels();
-            
-            Assert.IsTrue(labels.Count() == 1);
-            Assert.AreEqual(labels.First(), "SentViaUbuntu");
+
+            Assert.IsTrue(labels.Count() == 1, $"TestMethod: {MethodBase.GetCurrentMethod().Name} Failed, \nlabels count was {labels.Count()} when it should be 1.");
+            Assert.AreEqual(labels.First(), "SentViaUbuntu", $"TestMethod: {MethodBase.GetCurrentMethod().Name} Failed, \nfirst label was not the correct one.");
         }
     }
 }
